8299298e4117739d71b0b3750c7d130403eb6df4
Sudharshan Karthik <globusitsudharshan@gmail.com>
2023-08-02 22:46:25 +0530

    added index and made changes for app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,18 +1,11 @@
-from platform import java_ver
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def index1():
-    if request.method == "POST":
-        tweet = request.form["tweet"]
-        cleaned_tweet = re.sub(r"(@[A-Za-z0-9_]+)|[^\w\s\n]|#|http\S+", "", tweet.lower())
-        prediction = Nb_Model.predict([cleaned_tweet])[0]
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-        return render_template("index1.html", prediction=prediction)
 
-    return render_template("index1.html", prediction=None)
-
-if __name__ == "__main__":
+if __name__=="__main__":
     app.run(debug=True)
